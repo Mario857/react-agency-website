3923e0937bdbb82b6b38482c7f31a84a039bcb25
Mario Lučki <luckimario@gmail.com>
2021-11-17 23:09:11 +0100

    feat: update logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
+import * as moment from "moment";
 
 const Footer = () => {
-    return(
-        <footer>
-            <div className="container text-white">
-                <div className="row align-items-center">
-                    <div className="col-sm-12">
-                        <p className="mt-4 mb-4 title-font-2">
-                        ©2019 A. All rights reserved.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
-}
+  return (
+    <footer>
+      <div className="container text-white">
+        <div className="row align-items-center">
+          <div className="col-sm-12">
+            <p className="mt-4 mb-4 title-font-2">
+              @{moment().format("YYYY")} A. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
